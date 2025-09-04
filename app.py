78659b268d0ae90d79b63bf95ab81d38713d9511
Nevio Carcangiu <nevio@carcangiu.net>
2025-09-04 11:48:24 +0200

    Version with Icon

--- app.py
+++ app.py
@@ -4,25 +4,35 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
 
     data = None
     error = None
     temp = None
+    city = request.form.get("city")
+
 
     if request.method == "POST":
         try:
-            data = getData()
+            data = getData(city)
             if isinstance(data, str):
                 data = json.loads(data)
             temp = data.get("main", {}).get("temp")
+            icon_code = data["weather"][0]["icon"]
+            icon = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
+
 
 
         except Exception as e:
             error = f"Fehlrer beim Laden: {e}"
 
-    return render_template("index.html", error=error, temp=temp)
+    return render_template("result.html", error=error, temp=temp, city=city, icon=icon)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- weather_service.py
+++ weather_service.py
@@ -2,16 +2,17 @@ from dotenv import load_dotenv
 import os
 import requests
 
+
 load_dotenv()
 
 api_key = os.getenv("OPENWEATHER_API_KEY")
 
-location = "Zuerich"
-
-URL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + api_key + "&units=metric"
 
 
-def getData():
+
+def getData(city):
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
     r = requests.get(URL)
 
     r.raise_for_status()
